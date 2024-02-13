f31d811f4d4a17c90875d36f138caa4aa2eac495
Babatunde Balogun <balogunbabatunde42@gmail.com>
2024-02-13 16:09:27 +0100

    app

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState([]);
+  const [errors, setError] = useState(null);
+
+  useEffect(()=>{
+    fetch("https://hn.algolia.com/api/v1/search?query=redux")
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.hits)
+      setData(res.hits)
+    })
+    .catch(err => setError(err))
+  },[])
+
+
+  return (
+    <div className='grid grid-cols-4 gap-5'>
+     {data.map(item => (
+      <div className='bg-slate-300 p-5 flex flex-col items-center' key={item.story_id}>
+        <p>{item.title}</p>
+        <p>{item.author}</p>
+        <a target='_blank' className='bg-blue-400 w-fit px-3 py-1 rounded-md text-white' href={item.url} >Click to go to book</a>
+        {item._tags.map((tag, index) =>(
+          <div key={index}>
+            <p>{tag}</p>
+          </div>
+        ))}
+      </div>
+     ))}
+    </div>
+  )
+}
+
+export default App
